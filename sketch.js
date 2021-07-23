a13d0f0df491e07e1276b69587f9508dce5de84e
PurplePupsicle <87096139+PurplePupsicle@users.noreply.github.com>
2021-07-23 14:22:31 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,63 @@
+var garden,rabbit;
+var gardenImg,rabbitImg;
+var appleImg, leafImg;
+
+function preload(){
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+  appleImg = loadImage("apple.png");
+  leafImg = loadImage("redImage.png")
+}
+
+function setup(){
+  
+  createCanvas(400,400);
+  
+// Moving background
+garden=createSprite(200,200);
+garden.addImage(gardenImg);
+
+//creating boy running
+rabbit = createSprite(180,340,30,30);
+rabbit.addImage(rabbitImg);
+rabbit.scale =0.09;
+}
+
+
+function draw() {
+  background(0);
+  
+  edges= createEdgeSprites();
+  rabbit.collide(edges);
+  rabbit.x = mouseX;
+  drawSprites();
+
+  var select_sprites = Math.round(random(1,2));
+
+if (frameCount % 80 == 0) {
+  if (select_sprites == 1) {
+    createApples();
+  } 
+  else {
+    createLeaves();
+  }
+}
+}
+
+function createApples() {
+apple = createSprite(random(50,350),40,10,10);
+apple.addImage(appleImg);
+apple.scale = 0.1;
+apple.velocityY = 7;
+apple.lifetime = 200;
+
+}
+
+function createLeaves() {
+  leaf = createSprite(random(50,350),40,10,10);
+  leaf.addImage(leafImg);
+  leaf.scale = 0.1;
+  leaf.velocityY = 7;
+  leaf.lifetime = 200;
+  
+  }
